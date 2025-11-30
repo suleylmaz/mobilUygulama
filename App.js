989b005b05b96016295771833bb15d7ee0040a87
suleylmaz <suleyilmaz70@icloud.com>
2025-11-30 23:33:01 +0300

    First Commit3

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; 
 
-export default function App() {
+import HomeScreen from './src/screens/HomeScreen';
+import ReportsScreen from './src/screens/ReportsScreen';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            if (route.name === 'Ana Sayfa') {
+              iconName = focused ? 'timer' : 'timer-outline';
+            } else if (route.name === 'Raporlar') {
+              iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+            }
+            
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#38A169', 
+          tabBarInactiveTintColor: 'gray', 
+        })}
+      >
+        <Tab.Screen 
+            name="Ana Sayfa" 
+            component={HomeScreen} 
+            options={{ title: 'Odaklanma' }} 
+        />
+        <Tab.Screen 
+            name="Raporlar" 
+            component={ReportsScreen} 
+            options={{ title: 'Raporlar' }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
